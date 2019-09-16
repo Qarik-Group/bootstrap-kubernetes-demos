b695dd32a651fa1ee1ced0df12d959472628dc77
Dr Nic Williams <drnicwilliams@gmail.com>
2019-09-16 06:11:36 -0400

    exploring an idea for static failover of cf-domains.sh

--- resources/eirini/cf-domains.sh
+++ resources/eirini/cf-domains.sh
@@ -7,7 +7,12 @@
 #
 # See https://github.com/k14s/kwt/blob/master/docs/network.md#cheatsheet
 
+: ${CF_SYSTEM_DOMAIN:=scf.suse.dev}
 : ${CF_NAMESPACE:=scf}
 : ${API_IP:=$(kubectl get svc -n ${CF_NAMESPACE} scf-router-0 --template '{{.spec.clusterIP}}')}
+
+# If no connection to CF yet (kwt not running), then return static JSON
+#       {"scf.suse.dev":["35.184.47.142"]}
+# echo "{}" | jq -r --arg api_ip "$API_IP" --arg domain $CF_SYSTEM_DOMAIN '{($domain): [$api_ip]}'
 
 cf curl /v3/domains | jq -r --arg api_ip "$API_IP" '.resources | map({(.name): [$api_ip]}) | add'
