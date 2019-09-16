b3c7bcc8777b9d22acd138812985c9ad53386d8c
Dr Nic Williams <drnicwilliams@gmail.com>
2019-09-16 05:07:16 -0400

    allow alternate system domain for cf; add a load balancer for ingress

--- resources/eirini/kwt.sh
+++ resources/eirini/kwt.sh
@@ -4,12 +4,14 @@
 
 set -eu
 
+: ${CF_SYSTEM_DOMAIN:=scf.suse.dev}
+
 # api_ip=$(kubectl get svc -n scf scf-router-0 -o json | jq -r .spec.clusterIP)
 api_ip=$(kubectl get svc -n scf scf-router-0 --template '{{.spec.clusterIP}}')
-echo "Mapping *.scf.suse.dev to internal IP ${api_ip}..."
+echo "Mapping *.${CF_SYSTEM_DOMAIN} to internal IP ${api_ip}..."
 echo
 echo "Login with:"
 echo 'admin_password=$(kubectl get secret -n scf scf.var-cf-admin-password -o json | jq -r .data.password | base64 -D)'
-echo 'cf login -a https://api.scf.suse.dev --skip-ssl-validation -u admin -p "${admin_password}"'
+echo 'cf login -a https://api.${CF_SYSTEM_DOMAIN} --skip-ssl-validation -u admin -p "${admin_password}"'
 echo
-sudo -E kwt net start --dns-map scf.suse.dev=${api_ip} --namespace scf
+sudo -E kwt net start --dns-map ${CF_SYSTEM_DOMAIN}=${api_ip} --namespace scf
