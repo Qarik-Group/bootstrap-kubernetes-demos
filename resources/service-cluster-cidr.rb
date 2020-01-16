1a3a80104b1d14b671f3b3cede0b7296ad691f7e
Dr Nic Williams <drnicwilliams@gmail.com>
2020-01-16 08:50:01 -0700

    [kubecf] the cidrs can all be 0.0.0.0/0

--- resources/service-cluster-cidr.rb
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env ruby
-
-require 'json'
-$stderr.puts "+ kubectl get services -n kube-system -ojson"
-svc = JSON.parse(`kubectl get services -n kube-system -ojson`)
-clusterIPService = svc["items"].find {|i| i["spec"]["type"] == "ClusterIP"}
-ip = clusterIPService["spec"]["clusterIP"]
-
-puts "#{ip[/^(\d+)\.(\d+)/]}.0.0/16"
